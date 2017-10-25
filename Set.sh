af6dfc8c8db1d3adcc198edb9ee46b012f918865
C-Abner <chuangchita@gmail.com>
2017-10-25 10:23:58 +0900

    Update Set.sh

--- Set.sh
+++ Set.sh
@@ -1,17 +1,14 @@
-# install all of need
-echo $1
-$1 install -y sudo &&\
-sudo $1 update -y &&\
-sudo $1 install -y screen zsh vim git &&\
-# copy configs to the root dir
-sudo cp -r .bashrc ~/ &&\
-sudo cp -r .gitconfig ~/ &&\
-sudo cp -r .gitignore ~/ &&\
-sudo cp -r .oh-my-zsh ~/ &&\
-sudo cp -r .screenrc ~/ &&\
-sudo cp -r .vimrc ~/ &&\
+if [ -z "$1" ]; then
+	echo "please input args as yum or apt"
+	exit 1
+fi
+$1 install -y sudo 
+sudo $1 update -y 
+sudo $1 install -y screen zsh vim git 
+sudo cp -r .bashrc ~/ 
+sudo cp -r .gitconfig ~/ 
+sudo cp -r .gitignore ~/ 
+sudo cp -r .oh-my-zsh ~/ 
+sudo cp -r .screenrc ~/ 
+sudo cp -r .vimrc ~/
 sudo cp -r .zshrc ~/
-#git clone https://github.com/shajiquan/gitit-bigger ~/workspace/gitit;
-#cd ~/workspace/gitit;
-#cp sample.gitit.conf my-gitit.conf;
-#./run/run.sh start;
