245e1a49aa51f36c68ef883ea9c2dad10ba3d576
Test <Test@gmail.com>
2020-12-02 10:41:41 +0800

    [Fix] Set.sh Bug

--- Set.sh
+++ Set.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
 if [ -z "$1" ]; then
 	echo "please input args as yum or apt"
 	exit 1
 fi
-$1 install -y sudo 
-sudo $1 update -y 
-sudo $1 install -y screen zsh vim git 
-sudo cp -r .bashrc ~/ 
-sudo cp -r .gitconfig ~/ 
-sudo cp -r .gitignore ~/ 
-sudo cp -r .oh-my-zsh ~/ 
-sudo cp -r .screenrc ~/ 
+
+$1 install -y sudo
+sudo $1 update -y
+sudo $1 install -y screen zsh vim git
+sudo cp -r .bashrc ~/
+sudo cp -r .gitconfig ~/
+sudo cp -r .gitignore ~/
+sudo cp -r .oh-my-zsh ~/
+sudo cp -r .screenrc ~/
 sudo cp -r .vimrc ~/
 sudo cp -r .zshrc ~/
